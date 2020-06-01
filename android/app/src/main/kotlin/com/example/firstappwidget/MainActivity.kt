b1511697465f80194b0c0750547a926c2885c93a
Dhruv Rana <dhruvrana0000@gmail.com>
2020-06-01 13:41:06 +0530

    First details Commit

---
+++ android/app/src/main/kotlin/com/example/firstappwidget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firstappwidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MaterialApp(
+  home: Ninjacard()
+)
+);
+
+class Ninjacard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Ninja ID'),
+        backgroundColor: Colors.grey[850],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0 ,40.0 ,30.0 ,0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/dhruv.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 100.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Dhruv Rana',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 50.0),
+            Text(
+              'Current Learning Level',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              '4',
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 50.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.mail,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 20.0),
+                Text(
+                  'dhruvrana0000@gmai.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
